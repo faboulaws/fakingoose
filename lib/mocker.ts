010a2c04696c3f075e3b8869bcc48c638d5e1c2f
fabpulaws <faboulaws@gmail.com>
2021-05-15 03:19:23 +0300

    fix: #33 optional generic type inference

--- lib/mocker.ts
+++ lib/mocker.ts
@@ -28,7 +28,7 @@ class Mocker<T extends Document> {
     return this;
   }
 
-  generate(staticFields: Record<string,unknown> {}) {
+  generate(staticFields: Record<string,unknown> = {}) {
     return generate<T>(this.schema, {
       options: this.options,
       staticFields,
